fb2a3f22318510af0270e2f5456d8004d242968c
m.dobekidis <m.dobekidis@7linternational.com>
2020-04-27 13:36:53 +0300

    added express

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const express = require('express');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Albion Price Bot is online!'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
